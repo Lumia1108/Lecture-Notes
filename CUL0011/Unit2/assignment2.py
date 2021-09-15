b128dba364ec2f02e714cdeb7d87ff460d272122
JH <jh110821@hanyang.ac.kr>
2021-09-15 12:17:52 +0900

    창컴 2주차 실습과제 업로드

---
+++ CUL0011/Unit2/assignment2.py
@@ -0,0 +1,20 @@
+# 앞서 1번 문제를 변수 하나를 대상으로 복합 대입 연산자로 바꾸어 실행해보세요.
+
+a = 4
+b = 2
+
+a+=b
+print(a)
+a-=b
+print(a)
+a*=b
+print(a)
+a/=b
+print(a)
+a//=b
+print(a)
+a%=b
+print(a)
+a = 4
+a**=2
+print(a)
---
+++ CUL0011/Unit2/assignment4.py
@@ -0,0 +1,16 @@
+# 다음의 코드를 수정해서 아래의 실행결과 같이 출력되도록 수정해보세요.
+# 소스코드
+'''
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+'''
+# 실행결과
+# 1 2 3 4 5
+print(1, end=' ')
+print(2, end=' ')
+print(3, end=' ')
+print(4, end=' ')
+print(5, end=' ')
